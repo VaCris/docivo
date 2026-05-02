f5eb1e8dd16f3e83e3b25e10beac6fe52ef0e02b
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2026-05-02 08:35:26 -0500

    Add MergePage component to integrate MergeTool in the dashboard

---
+++ app/dashboard/merge/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { MergeTool } from "@/features/dashboard/components/MergeTool/MergeTool";
+
+export default function MergePage() {
+    return (
+        <div className="mx-auto max-w-6xl">
+            <MergeTool />
+        </div>
+    );
+}
