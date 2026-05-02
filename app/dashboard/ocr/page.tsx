d6eed8737cb3863c5a8a0d35c2c54bbe21afdac0
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2026-05-02 08:35:36 -0500

    Add OcrPage component to integrate OcrTool in the dashboard

---
+++ app/dashboard/ocr/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { OcrTool } from "@/features/dashboard/components/OcrTool/OcrTool";
+
+export default function OcrPage() {
+    return (
+        <div className="mx-auto max-w-6xl h-full">
+            <OcrTool />
+        </div>
+    );
+}
