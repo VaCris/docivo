223f7be9e3be343892c04011374ecc8521d521e3
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2026-05-02 08:34:31 -0500

    Add ConvertImageToPdfPage component to handle image to PDF conversion

---
+++ app/dashboard/convert-image-to-pdf/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ImageToPdfTool } from "@/features/dashboard/components/ImageToPdfTool/ImageToPdfTool";
+
+export default function ConvertImageToPdfPage() {
+    return (
+        <div className="mx-auto max-w-6xl h-full">
+            <ImageToPdfTool />
+        </div>
+    );
+}
