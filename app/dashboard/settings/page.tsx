510a5c49888967f85132867f9302251b1f64be35
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2026-05-02 08:35:42 -0500

    Add SettingsPage component to integrate SettingsView in the dashboard

---
+++ app/dashboard/settings/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { SettingsView } from "@/features/dashboard/components/SettingsView/SettingsView";
+
+export default function SettingsPage() {
+    return (
+        <div className="mx-auto max-w-6xl h-full">
+            <SettingsView />
+        </div>
+    );
+}
