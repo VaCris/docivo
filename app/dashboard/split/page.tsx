0bc372e27e1b15c237e731ed0272cc349daa6246
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2026-05-02 08:35:49 -0500

    Add SplitPage component to integrate SplitTool in the dashboard

---
+++ app/dashboard/split/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { SplitTool } from "@/features/dashboard/components/SplitTool/SplitTool";
+
+export default function SplitPage() {
+    return (
+        <div className="mx-auto max-w-6xl h-full">
+            <SplitTool />
+        </div>
+    );
+}
