c9f816c1139a629ee352085801c8726507a49013
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2026-05-02 08:34:37 -0500

    Add ConvertPdfToWordPage component to facilitate PDF to Word conversion

---
+++ app/dashboard/convert-pdf-to-word/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { PdfToWordTool } from "@/features/dashboard/components/PdfToWordTool/PdfToWordTool";
+
+export default function ConvertPdfToWordPage() {
+    return (
+        <div className="mx-auto max-w-6xl h-full">
+            <PdfToWordTool />
+        </div>
+    );
+}
