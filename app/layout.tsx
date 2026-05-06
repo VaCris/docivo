dac7d20e0004490ab7bbf6c81c1eb0fef1bd3eb7
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2026-05-06 11:54:50 -0500

    Add suppressHydrationWarning to RootLayout for improved hydration handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="es" className="scroll-smooth">
+    <html lang="es" className="scroll-smooth" suppressHydrationWarning>
       <body suppressHydrationWarning className={`${plusJakarta.className} antialiased`}>
         <LanguageProvider>
           {children}
