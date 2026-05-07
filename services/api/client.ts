82089afa0c23e867b7c4aef552bebd14ebd60a47
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2026-05-07 15:54:49 -0500

    Update API base URL to use NEXT_PUBLIC_API_URL for client configuration

--- services/api/client.ts
+++ services/api/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_API_URL;
+const baseURL = process.env.NEXT_PUBLIC_API_URL;
 
 if (!baseURL) {
     throw new Error("NEXT_API_URL is not defined");
