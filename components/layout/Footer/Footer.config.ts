0881eab8d917073c9176d4ecba0a3fca6e628f5e
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2026-05-07 17:19:45 -0500

    Update footer configuration and styles for improved layout and aesthetics

--- components/layout/Footer/Footer.config.ts
+++ components/layout/Footer/Footer.config.ts
@@ -1,11 +1,11 @@
 export const FOOTER_CONFIG = {
     columns: {
         tools: [
-            { id: 'merge', href: '/tools/merge' },
-            { id: 'split', href: '/tools/split' },
-            { id: 'ocr', href: '/tools/ocr' },
-            { id: 'pdfToWord', href: '/tools/pdf-to-word' },
-            { id: 'imageToPdf', href: '/tools/image-to-pdf' }
+            { id: 'merge', href: '/dashboard/merge' },
+            { id: 'split', href: '/dashboard/split' },
+            { id: 'ocr', href: '/dashboard/ocr' },
+            { id: 'pdfToWord', href: '/dashboard/convert-pdf-to-word' },
+            { id: 'imageToPdf', href: '/dashboard/convert-image-to-pdf' }
         ],
         project: [
             { id: 'architecture', href: '#architecture' },
