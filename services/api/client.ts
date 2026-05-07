594e3bb50202cccdc0949a8c8be235a55985fabb
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2026-05-07 16:14:55 -0500

    Fix base URL fallback and correct error message for API URL definition

--- services/api/client.ts
+++ services/api/client.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL;
+const baseURL = process.env.NEXT_PUBLIC_API_URL || "https://docivo-api.studios-tkoh.online/api/v1";
 
 if (!baseURL) {
-    throw new Error("NEXT_API_URL is not defined");
+    throw new Error("NEXT_PUBLIC_API_UR is not defined");
 }
 
 export const api = axios.create({
