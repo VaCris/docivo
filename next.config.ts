69f5b64b5afb2c35dfb70a91cb0169a51806476f
Bryan Alexander Vidal Crispin <144495404+VaCris@users.noreply.github.com>
2026-05-15 08:43:09 -0500

    Update Next.js configuration to enable static export, enforce trailing slashes, and optimize image handling

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
   allowedDevOrigins: ['192.168.1.7:3001', 'localhost:3001'],
 };
 
